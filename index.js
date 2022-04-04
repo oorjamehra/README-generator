ed540fff9582fc12120f2081843eb2ee85dc0387
Oorja Mehra <oorjamehra@gmail.com>
2022-04-03 23:19:23 -0500

    questions added

--- index.js
+++ index.js
@@ -1,8 +1,78 @@
 // Inquirer package installed
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // Questions to prompt user for info to be entered on README file
-const questions = ["Project Title", "Description", "Installation Instructions", "Usage Info", 
-"Contribution Guidelines", "Test Instructions", "License","Username", "Email"];
+const questions = [
+    {
+        type: 'input',
+        message: "What is your project title?",
+        name: "Title",
+        default: "README-generator",
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log("A project title is required");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "Please write a short description of your project.",
+        name: "Description",
+        default: "Project Description",
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log("A project description is required");
+            }
+            return true;
+    },
+    {
+        type: 'input',
+        message: "Installation instructions.",
+        name: "Installation",
+        default: "Installation Instructions",
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log("Please enter installation instructions");
+            }
+            return true;
+    },
+    {
+        type: 'input',
+        message: "Please a section for usage of project .",
+        name: "Usage",
+        default: "Usage",
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log("Please include usage info");
+            }
+            return true;
+    },
+    {
+        type: 'input',
+        message: "Please include contributing guidelines",
+        name: "Contributing",
+        default: "Contribution guidelines",
+        validate: function(answer) {
+            if (answer.length < 1) {
+                return console.log("A project description is required");
+            }
+            return true;
+    },
+    {
+        type: 'input',
+        message: "Please include instructions for how to run tests",
+        name: "Test",
+        default: "Test instructions",
+        validate: function(answer) {
+            if (answer.length <n 1) {
+                return console.log("Test instructions required");
+            }
+            return true;
+    }
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
